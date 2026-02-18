c496439933b741e91c2a687cb1e90e2e7a47dca7
Krishnasairaj <krishnasairaj.ponneboina@gmail.com>
2026-02-18 18:19:13 +0530

    Started Implementing TC_023(Form submission for business)

---
+++ src/test/java/testcases/TC_023.java
@@ -0,0 +1,12 @@
+package testcases;
+
+import basetest.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TC_023 extends BaseTest {
+    WebDriver driver;
+    WebDriverWait wait;
+    By bussinessLink= By.xpath("//span[text()=' Businesses']");
+}
