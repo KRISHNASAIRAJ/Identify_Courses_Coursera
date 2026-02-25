21dd1c5c8cd19a25304af890e01039e3ed66fa8f
Krishnasairaj <krishnasairaj.ponneboina@gmail.com>
2026-02-25 15:40:07 +0530

    Updated variable TC_022 TC_023

--- src/test/java/testcases/TC_022_FormSubmissionWithValidData.java
+++ src/test/java/testcases/TC_022_FormSubmissionWithValidData.java
@@ -11,7 +11,7 @@ public class TC_022_FormSubmissionWithValidData extends BaseTest {
     void formSubmissionWithValidData() throws InterruptedException{
         BusinessFormPage businessFormPage =new BusinessFormPage(driver,wait);
         businessFormPage.moveToFormArea();
-        businessFormPage.formFilling("Krishna","Sai","kri@cognizant.com","1234567890",1,"SDE",1,2,"India","Telangana");
+        businessFormPage.formFilling("Krishna","Sai","kri@cognizant.com","1234567890",1,"SDE",1,2,"India","Telangana","CTS");
         Assert.assertFalse(businessFormPage.emailCheck());
         Assert.assertTrue(businessFormPage.formSubmissionStatus());
     }
--- src/test/java/testcases/TC_023_FormSubmissionWithInvalidData.java
+++ src/test/java/testcases/TC_023_FormSubmissionWithInvalidData.java
@@ -11,7 +11,7 @@ public class TC_023_FormSubmissionWithInvalidData extends BaseTest {
     void formSubmissionWithInvalidData() throws InterruptedException{
         BusinessFormPage businessFormPage =new BusinessFormPage(driver,wait);
         businessFormPage.moveToFormArea();
-        businessFormPage.formFilling("Krishna","Sai","kri@gmail","1234567890",1,"SDE",1,2,"India","Telangana");
+        businessFormPage.formFilling("Krishna","Sai","kri@gmail","1234567890",1,"SDE",1,2,"India","Telangana","CTS");
         Assert.assertTrue(businessFormPage.emailCheck());
         Assert.assertFalse(businessFormPage.formSubmissionStatus());
     }
